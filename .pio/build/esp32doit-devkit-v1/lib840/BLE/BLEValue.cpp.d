c7ac1f0e9e1439c485cc5004f7d8a80cf45785cd
DQuangVN <idoubtyouknowmyemail@gmail.com>
2023-11-05 20:34:35 +0700

    Seccond commit

---
+++ .pio/build/esp32doit-devkit-v1/lib840/BLE/BLEValue.cpp.d
@@ -0,0 +1,13 @@
+.pio/build/esp32doit-devkit-v1/lib840/BLE/BLEValue.cpp.o: \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEValue.cpp \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEValue.h \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-log.h \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_timer/include/esp_timer.h \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/log/include/esp_log.h \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_rom/include/esp_rom_sys.h \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/soc/esp32/include/soc/reset_reasons.h \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_rom/include/esp32/rom/ets_sys.h \
+ /home/sussypikachu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/log/include/esp_log_internal.h
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Soccer_bot_esp32
-# Issue:
-Disconnected 
+# Soccer_bot_esp32_mecanum_Wheels
+
+# Resources:
+https://github.com/robot-soccer-kit/robot-soccer-kit
+https://randomnerdtutorials.com/esp32-dc-motor-l298n-motor-driver-control-speed-direction/
+https://esp32io.com/tutorials/esp32-dc-motor
+https://en.wikipedia.org/wiki/Mecanum_wheel
